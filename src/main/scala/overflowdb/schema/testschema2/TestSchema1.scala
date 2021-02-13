d5aa4455d2cef08666350e1d29a359b81005510d
Michael Pollmeier <michael@michaelpollmeier.com>
2021-02-13 17:18:18 +1300

    take 4: a bit over engineered ?

--- src/main/scala/overflowdb/schema/testschema2/TestSchema1.scala
+++ src/main/scala/overflowdb/schema/testschema2/TestSchema1.scala
@@ -1,21 +1,27 @@
 package overflowdb.schema.testschema2
 
 import overflowdb.codegen.CodeGen
-import overflowdb.schema.{Cardinality, Constant, SchemaBuilder}
+import overflowdb.schema.testschema2.Base.Schema
+import overflowdb.schema.{Cardinality, Constant, Property, SchemaBuilder}
 
 import java.io.File
 
 // TODO create integration test from this
 object TestSchema2 extends App {
   val builder = new SchemaBuilder("io.shiftleft.codepropertygraph.generated")
-  val base = Base(builder)
+//
+  val base = Base.addAll(builder)
   base.name
 //  val javaSpecific = new JavaSpecific(builder, base)
 //  new CodeGen(builder.build).run(new File("target"))
 }
 
-object Base{
-  def apply(builder: SchemaBuilder) = new Schema(builder)
+trait PartialSchema[T] {
+  def addAll(builder: SchemaBuilder): T
+}
+
+object Base extends PartialSchema[Base.Schema] {
+  override def addAll(builder: SchemaBuilder) = new Schema(builder)
   private class Schema(builder: SchemaBuilder) {
     val name = builder.addNodeProperty("NAME", "string", Cardinality.One, "Name of represented object, e.g., method name (e.g. \"run\")")
   }
