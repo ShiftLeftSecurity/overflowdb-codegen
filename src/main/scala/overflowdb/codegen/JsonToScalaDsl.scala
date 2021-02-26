63a399451b5218a53ec51cfa2b1292256bba0ccd
Michael Pollmeier <michael@michaelpollmeier.com>
2021-02-26 16:32:30 +1300

    printlns

--- src/main/scala/overflowdb/codegen/JsonToScalaDsl.scala
+++ src/main/scala/overflowdb/codegen/JsonToScalaDsl.scala
@@ -1,20 +1,13 @@
 package overflowdb.codegen
 
-import better.files._
-
 object JsonToScalaDsl extends App {
   val schema = new Schema("testschema.json")
-  val out = File("out")
-  if (out.exists) out.delete()
-  out.createFile()
 
-  out.appendLine("// node properties")
+  println("// node properties")
   schema.nodeKeys.foreach { key =>
-    out.appendLine(
+    println(
       s"""val ${Helpers.camelCase(key.name)} = builder.addNodeProperty("${key.name}", "String", Cardinality.One,
         "Name of represented object, e.g., method name (e.g. \"run\")")"""
     )
   }
-
-  out.lines.foreach(println)
 }
