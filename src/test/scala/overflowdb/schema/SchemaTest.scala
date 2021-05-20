ec38842115bfa08a320676979aade458064d4893
Michael Pollmeier <michael@michaelpollmeier.com>
2021-05-20 21:23:34 +1200

    bring in base nodes to the mix

--- src/test/scala/overflowdb/schema/SchemaTest.scala
+++ src/test/scala/overflowdb/schema/SchemaTest.scala
@@ -7,8 +7,10 @@ class SchemaTest extends AnyWordSpec with Matchers {
 
   "NeighborInfo.deriveNeighborNodeType" when {
     val defaultNeighborNodeType = "StoredNode"
-    val nodeA1 = new NodeType(name = "A1", comment = None, SchemaInfo.Unknown)
-    val nodeB2 = new NodeType(name = "B1", comment = None, SchemaInfo.Unknown)
+    val baseNodeA = new NodeBaseType(name = "BASE_A", comment = None, SchemaInfo.Unknown)
+    val baseNodeB = new NodeBaseType(name = "BASE_B", comment = None, SchemaInfo.Unknown)
+    val nodeA1 = new NodeType(name = "A1", comment = None, SchemaInfo.Unknown).extendz(baseNodeA)
+    val nodeB2 = new NodeType(name = "B1", comment = None, SchemaInfo.Unknown).extendz(baseNodeB)
 
     "having no (known) neighbor" in {
       neighborInfoWith(Seq.empty).deriveNeighborNodeType shouldBe defaultNeighborNodeType
@@ -16,14 +18,17 @@ class SchemaTest extends AnyWordSpec with Matchers {
 
     "having one neighbor" in {
       neighborInfoWith(Seq(nodeA1)).deriveNeighborNodeType shouldBe nodeA1.className
+      neighborInfoWith(Seq(baseNodeA)).deriveNeighborNodeType shouldBe baseNodeA.className
     }
 
     "having multiple neighbors with same type (e.g. different edges to same node type)" in {
       neighborInfoWith(Seq(nodeA1, nodeA1)).deriveNeighborNodeType shouldBe nodeA1.className
+      neighborInfoWith(Seq(baseNodeA, baseNodeA)).deriveNeighborNodeType shouldBe baseNodeA.className
     }
 
     "having different, unrelated neighbors" in {
       neighborInfoWith(Seq(nodeA1, nodeB2)).deriveNeighborNodeType shouldBe defaultNeighborNodeType
+      neighborInfoWith(Seq(baseNodeA, baseNodeB)).deriveNeighborNodeType shouldBe defaultNeighborNodeType
     }
 
     def neighborInfoWith(nodes: Seq[AbstractNodeType]): NeighborInfo =
