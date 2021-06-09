2cfe4f316256266fd0a02c9a028f5dea3e35a2f0
Michael Pollmeier <michael@michaelpollmeier.com>
2021-06-09 16:23:10 +0200

    BaseType edges: in/out: test iterator and traversals

--- integration-tests/tests/src/test/scala/Schema02Test.scala
+++ integration-tests/tests/src/test/scala/Schema02Test.scala
@@ -15,5 +15,23 @@ class Schema02Test extends AnyWordSpec with Matchers {
     BaseNode.Edges.In shouldBe Array(Edge2.Label)
   }
 
+  "working with a concrete sample graph" can {
+    val graph = Graph.open(Config.withDefaults, nodes.Factories.allAsJava, edges.Factories.allAsJava)
+
+    val node1 = graph.addNode(Node1.Label, Node1.PropertyNames.Name, "node 01")
+    val node2 = graph.addNode(Node2.Label, PropertyNames.NAME, "node 02", PropertyNames.ORDER, 3)
+    node1.addEdge(Edge1.Label, node2)
+    node2.addEdge(Edge2.Label, node1, PropertyNames.NAME, "edge 02")
+
+    "lookup and traverse nodes/edges/properties" in {
+      def baseNodeTraversal = graph.nodes(Node1.Label).cast[BaseNode]
+      val baseNode = baseNodeTraversal.head
+      baseNode.edge2In.l shouldBe Seq(node2)
+      baseNode.edge1Out.l shouldBe Seq(node2)
+      baseNode._node2ViaEdge2In shouldBe node2
+      baseNode._node2ViaEdge1Out.l shouldBe Seq(node2)
+    }
+  }
+
 
 }
