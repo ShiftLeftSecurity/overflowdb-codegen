f049de5ef55aa709392d9f7effa355d2c58e2e49
Michael Pollmeier <michael@michaelpollmeier.com>
2021-06-18 17:47:42 +0200

    simplified further

--- integration-tests/schemas/src/main/scala/TestSchema03.scala
+++ integration-tests/schemas/src/main/scala/TestSchema03.scala
@@ -4,10 +4,9 @@
 class TestSchema03 extends TestSchema {
   val abstractNode1 = builder.addNodeBaseType(name = "ABSTRACT_NODE1")
   val nodeExt1 = builder.addNodeType(name = "NODE_EXT1").extendz(abstractNode1)
-  val nodeExt2 = builder.addNodeType(name = "NODE_EXT2").extendz(abstractNode1)
   val otherNode = builder.addNodeType(name = "OTHER_NODE")
   val edge1 = builder.addEdgeType(name = "EDGE1")
 
   nodeExt1.addOutEdge(edge = edge1, inNode = abstractNode1)
-  otherNode.addOutEdge(edge = edge1, inNode = nodeExt2)
+  otherNode.addOutEdge(edge = edge1, inNode = nodeExt1)
 }
