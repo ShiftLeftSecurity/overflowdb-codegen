c3afa5b64b0a9358ec6e6db3e67721f659d358ea
Michael Pollmeier <michael@michaelpollmeier.com>
2021-07-05 09:40:01 +0200

    default value must appear in valueMap - this was different to before

--- integration-tests/tests/src/test/scala/Schema04Test.scala
+++ integration-tests/tests/src/test/scala/Schema04Test.scala
@@ -8,7 +8,6 @@ import testschema04.nodes._
 import testschema04.traversal._
 
 class Schema04Test extends AnyWordSpec with Matchers {
-
   "default property values" in {
     val graph = Graph.open(Config.withDefaults, nodes.Factories.allAsJava, edges.Factories.allAsJava)
 
@@ -16,25 +15,32 @@ class Schema04Test extends AnyWordSpec with Matchers {
     val node2 = graph.addNode(Node1.Label).asInstanceOf[Node1]
     val edge1 = node1.addEdge(Edge1.Label, node2).asInstanceOf[Edge1]
 
-    // TODO define defaults in codegen?
     node1.bool shouldBe false
     node1.str shouldBe "<[empty]>"
     node1.byte shouldBe 0
     node1.short shouldBe 0
     node1.int shouldBe 0
     node1.long shouldBe 0
-    node1.float shouldBe Float.NaN
-    node1.double shouldBe Double.NaN
+    node1.float.isNaN shouldBe true
+    node1.double.isNaN shouldBe true
     node1.char shouldBe '?'
-    node1.valueMap shouldBe Map(
-      "STR" -> "<[empty]>",
-      "TODO" -> "fill all others"
-    )
+    node1.valueMap.get("STR") shouldBe "<[empty]>"
+    node1.valueMap.get("BOOL") shouldBe false
 
-    // TODO repeat for edge, nodeTrav, edgeTrav
-    // TODO for all other property value types
-    // TODO for custom defined properties
+//    edge1.bool shouldBe false
+//    node1.str shouldBe "<[empty]>"
+//    node1.byte shouldBe 0
+//    node1.short shouldBe 0
+//    node1.int shouldBe 0
+//    node1.long shouldBe 0
+//    node1.float.isNaN shouldBe true
+//    node1.double.isNaN shouldBe true
+//    node1.char shouldBe '?'
+//    node1.valueMap.get("STR") shouldBe "<[empty]>"
+//    node1.valueMap.get("BOOL") shouldBe false
 
+    // TODO repeat for nodeTrav, edgeTrav
+    // TODO add custom defined properties
 
 //      def baseNodeTraversal = graph.nodes(Node1.Label).cast[BaseNode]
 //      val baseNode = baseNodeTraversal.head
