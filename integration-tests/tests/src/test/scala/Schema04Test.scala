7e2ae6ecf8d2105bffe7e92fd9eed0c14a2a828f
Michael Pollmeier <michael@michaelpollmeier.com>
2021-07-13 22:01:54 +0200

    TestSchema04: add a second test where we set all those properties
    not quite done yet...

--- integration-tests/tests/src/test/scala/Schema04Test.scala
+++ integration-tests/tests/src/test/scala/Schema04Test.scala
@@ -64,4 +64,77 @@ class Schema04Test extends AnyWordSpec with Matchers {
     node1Trav.property(Node1.Properties.Str).head shouldBe "<[empty]>"
     edge1Trav.property(Edge1.Properties.Str).head shouldBe "<[empty]>"
   }
+
+  "defined property values" in {
+    val graph = Graph.open(Config.withDefaults, nodes.Factories.allAsJava, edges.Factories.allAsJava)
+
+    val node1 = graph.addNode(Node1.Label).asInstanceOf[Node1]
+    val node2 = graph.addNode(Node1.Label).asInstanceOf[Node1]
+    val edge1 = node1.addEdge(Edge1.Label, node2).asInstanceOf[Edge1]
+    val properties = Seq(
+      Properties.BOOL.of(false),
+      Properties.STR.of("foo"),
+      Properties.BYTE.of(100: Byte),
+      Properties.SHORT.of(101: Short),
+      Properties.INT.of(102),
+      Properties.LONG.of(103),
+      Properties.FLOAT1.of(Float.NaN),
+      Properties.FLOAT2.of(104.4f),
+      Properties.DOUBLE1.of(Double.NaN),
+      Properties.DOUBLE2.of(105.5),
+      Properties.CHAR.of('Z'),
+      // Properties.INT_LIST, ,
+      // Properties.INT_LIST_INDEXED, ,
+    )
+    properties.foreach(node1.setProperty)
+    properties.foreach(edge1.setProperty)
+
+    node1.bool shouldBe false
+    node1.str shouldBe "foo"
+    node1.byte shouldBe 100
+    node1.short shouldBe 101
+    node1.int shouldBe 102
+    node1.long shouldBe 103
+    node1.float1.isNaN shouldBe true
+    node1.float2 shouldBe 104.4f
+    node1.double1.isNaN shouldBe true
+    node1.double2 shouldBe 105.5
+    node1.char shouldBe 'Z'
+//    node1.intList.size shouldBe 0
+//    node1.intListIndexed.size shouldBe 0
+    node1.propertyKeys().contains("STR") shouldBe true
+    node1.propertyDefaultValue("STR") shouldBe "<[empty]>"
+    node1.propertyDefaultValue("DOESNT_EXIST") shouldBe null
+    node1.property(Node1.Properties.Str) shouldBe "foo"
+    node1.property("DOESNT_EXIST") shouldBe null
+    node1.propertiesMap.get("STR") shouldBe "foo"
+    node1.get.propertiesMapWithoutDefaults.get(PropertyNames.STR) shouldBe "foo"
+
+    edge1.bool shouldBe false
+    edge1.str shouldBe "foo"
+    edge1.byte shouldBe (100: Byte)
+    edge1.short shouldBe 101
+    edge1.int shouldBe 102
+    edge1.long shouldBe 103
+    edge1.float1.isNaN shouldBe true
+    edge1.float2 shouldBe 104.4f
+    edge1.double1.isNaN shouldBe true
+    edge1.double2 shouldBe 105.5
+    edge1.char shouldBe 'Z'
+    //    edge1.intList.size shouldBe 0
+    //    edge1.intListIndexed.size shouldBe 0
+    edge1.propertyKeys().contains("STR") shouldBe true
+    edge1.propertyDefaultValue("STR") shouldBe "<[empty]>"
+    edge1.propertyDefaultValue("DOESNT_EXIST") shouldBe null
+    edge1.property(Node1.Properties.Str) shouldBe "foo"
+    edge1.property("DOESNT_EXIST") shouldBe null
+    edge1.propertiesMap.get("STR") shouldBe "foo"
+
+    def node1Trav = graph.nodes(Node1.Label).cast[Node1]
+    def edge1Trav = graph.edges(Edge1.Label).cast[Edge1]
+    node1Trav.str.head shouldBe "foo"
+    node1Trav.intList.l shouldBe Seq.empty //TODO
+    node1Trav.property(Node1.Properties.Str).head shouldBe "foo"
+    edge1Trav.property(Edge1.Properties.Str).head shouldBe "foo"
+  }
 }
