333804c2e944e7c510e4e5fa0bb31d4c60a54f4e
Michael Pollmeier <michael@michaelpollmeier.com>
2021-11-11 11:37:07 +0100

    fix build setup

--- project/Build.scala
+++ project/Build.scala
@@ -11,8 +11,8 @@ object Projects {
   /** scala cross version settings for codegen:
     * we need scala 2.12 for the sbt plugin and 2.13 for everything else */
   lazy val codegen = BaseProject(project.in(file("codegen"))).cross
-  lazy val codegen_2_12 = codegen(scala_2_12)
-  lazy val codegen_2_13 = codegen(scala_2_13)
+  lazy val codegen_2_12 = codegen(Versions.scala_2_12)
+  lazy val codegen_2_13 = codegen(Versions.scala_2_13)
 
   lazy val sbtPlugin = project.in(file("sbt-overflowdb"))
   lazy val integrationTests = project.in(file("integration-tests"))
