7d8e7280ab4c33225e36f249b578df8210e8be23
Michael Pollmeier <michael@michaelpollmeier.com>
2021-11-11 11:40:17 +0100

    upgrade scala to 2.13.7 and release sequentially (#111)
    * upgrade scala to 2.13.7 and release sequentially
    
    * refactor build
    
    * upgrade odb
    
    * fix build setup

--- project/Build.scala
+++ project/Build.scala
@@ -1,17 +1,19 @@
 import sbt._
 import com.lucidchart.sbtcross.BaseProject
 
+object Versions {
+  val overflowdb = "1.69"
+  val scala_2_12 = "2.12.15"
+  val scala_2_13 = "2.13.7"
+}
+
 object Projects {
   /** scala cross version settings for codegen:
     * we need scala 2.12 for the sbt plugin and 2.13 for everything else */
   lazy val codegen = BaseProject(project.in(file("codegen"))).cross
-  lazy val codegen_2_12 = codegen("2.12.4")
-  lazy val codegen_2_13 = codegen("2.13.6")
+  lazy val codegen_2_12 = codegen(Versions.scala_2_12)
+  lazy val codegen_2_13 = codegen(Versions.scala_2_13)
 
   lazy val sbtPlugin = project.in(file("sbt-overflowdb"))
   lazy val integrationTests = project.in(file("integration-tests"))
-}
-
-object Versions {
-  val overflowdb = "1.62"
 }
