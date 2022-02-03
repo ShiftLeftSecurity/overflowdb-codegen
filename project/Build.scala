8ece40b593cd751f1648ae26624566b6a02a3d2f
Bernhard <bernhard@shiftleft.io>
2022-02-03 15:24:26 +0100

    fix version

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 import com.lucidchart.sbtcross.BaseProject
 
 object Versions {
-  val overflowdb = "1.107+11-43298b39+20220201-2337"
+  val overflowdb = "1.110+13-a9ede5cc"
   val scala_2_12 = "2.12.15"
   val scala_2_13 = "2.13.8"
   val scala_3 = "3.1.1"
