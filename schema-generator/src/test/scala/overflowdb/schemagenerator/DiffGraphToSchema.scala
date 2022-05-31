92bb4b3fe884ac3bef60194b2309f7a96406240b
Michael Pollmeier <michael@michaelpollmeier.com>
2022-05-31 15:49:10 +0200

    more

--- schema-generator/src/test/scala/overflowdb/schemagenerator/DiffGraphToSchema.scala
+++ schema-generator/src/test/scala/overflowdb/schemagenerator/DiffGraphToSchema.scala
@@ -4,6 +4,8 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import overflowdb.BatchedUpdate._
 
+import scala.jdk.CollectionConverters.IterableHasAsJava
+
 class DiffGraphToSchemaTest extends AnyWordSpec with Matchers {
   val domainName = "SampleDomain"
   val schemaPackage = "odb.sample.schema"
@@ -31,6 +33,21 @@ class DiffGraphToSchemaTest extends AnyWordSpec with Matchers {
         "boolProp2", true: java.lang.Boolean,
         "byteProp1", 1: Byte,
         "byteProp2", java.lang.Byte.valueOf(2: Byte),
+        "shortProp1", 3: Short,
+        "shortProp2", java.lang.Short.valueOf(4: Short),
+        "intProp1", 5: Int,
+        "intProp2", 6: Integer,
+        "longProp1", 7: Long,
+        "longProp2", 8: java.lang.Long,
+        "floatProp1", 9.1f: Float,
+        "floatProp2", 10.1f: java.lang.Float,
+        "doubleProp1", 11.1f: Double,
+        "doubleProp2", 12.1f: java.lang.Double,
+        "charProp1", 'a': Char,
+        "charProp2", 'b': Character,
+        "listProp1", Array("string1", "string2"),
+        "listProp2", Seq("string1", "string2"),
+        "listProp3", Seq("string1", "string2").asJava,
       )
       .build()
 
@@ -41,9 +58,22 @@ class DiffGraphToSchemaTest extends AnyWordSpec with Matchers {
     result should include(s"""val boolProp2 = builder.addProperty(name = "boolProp2", valueType = ValueType.Boolean)""")
     result should include(s"""val byteProp1 = builder.addProperty(name = "byteProp1", valueType = ValueType.Byte)""")
     result should include(s"""val byteProp2 = builder.addProperty(name = "byteProp2", valueType = ValueType.Byte)""")
-    result should include("""val thing = builder.addNodeType(name = "Thing").addProperties(boolProp1, boolProp2, byteProp1, byteProp2, stringProp)""")
-    result should include("""val thing = builder.addNodeType(name = "Thing").addProperties(boolProp1, boolProp2, byteProp1, byteProp2, stringProp)""")
-
+    result should include(s"""val shortProp1 = builder.addProperty(name = "shortProp1", valueType = ValueType.Short)""")
+    result should include(s"""val shortProp2 = builder.addProperty(name = "shortProp2", valueType = ValueType.Short)""")
+    result should include(s"""val intProp1 = builder.addProperty(name = "intProp1", valueType = ValueType.Int)""")
+    result should include(s"""val intProp2 = builder.addProperty(name = "intProp2", valueType = ValueType.Int)""")
+    result should include(s"""val longProp1 = builder.addProperty(name = "longProp1", valueType = ValueType.Long)""")
+    result should include(s"""val longProp2 = builder.addProperty(name = "longProp2", valueType = ValueType.Long)""")
+    result should include(s"""val floatProp1 = builder.addProperty(name = "floatProp1", valueType = ValueType.Float)""")
+    result should include(s"""val floatProp2 = builder.addProperty(name = "floatProp2", valueType = ValueType.Float)""")
+    result should include(s"""val doubleProp1 = builder.addProperty(name = "doubleProp1", valueType = ValueType.Double)""")
+    result should include(s"""val doubleProp2 = builder.addProperty(name = "doubleProp2", valueType = ValueType.Double)""")
+    result should include(s"""val charProp1 = builder.addProperty(name = "charProp1", valueType = ValueType.Char)""")
+    result should include(s"""val charProp2 = builder.addProperty(name = "charProp2", valueType = ValueType.Char)""")
+    result should include(s"""val listProp1 = builder.addProperty(name = "listProp1", valueType = ValueType.List)""")
+    result should include(s"""val listProp2 = builder.addProperty(name = "listProp2", valueType = ValueType.List)""")
+    result should include(s"""val listProp3 = builder.addProperty(name = "listProp3", valueType = ValueType.List)""")
+    result should include("""val thing = builder.addNodeType(name = "Thing").addProperties(boolProp1, boolProp2, byteProp1, byteProp2, charProp1, charProp2, doubleProp1, doubleProp2, floatProp1, floatProp2, intProp1, intProp2, listProp1, listProp2, listProp3, longProp1, longProp2, shortProp1, shortProp2, stringProp)""")
   }
 
 //  "schema with ambiguities in property names from diffgraph" in {
